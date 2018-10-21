ccf5d2db604d1a1e14585e6a975a35f00c7013b6
jovimassimiliano <massimiliano.giovanni@yahoo.com>
2018-10-21 22:00:21 +0700

    initialize commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
+import configureStore from './src/store';
+
+const store = configureStore();
+
+const RootApp = () => (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => RootApp);
